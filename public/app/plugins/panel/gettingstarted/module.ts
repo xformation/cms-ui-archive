e1881cf8b3dc2d3da8622615a313ede434ae071b
papu bhattacharya <papu.bhattacharya@synectiks.com>
2019-01-16 18:01:49 +0530

    Admin Home Dashboard is changed

--- public/app/plugins/panel/gettingstarted/module.ts
+++ public/app/plugins/panel/gettingstarted/module.ts
@@ -39,10 +39,10 @@ class GettingStartedPanelCtrl extends PanelCtrl {
     });
 
     this.steps.push({
-      title: 'Create your first dashboard',
+      title: 'Setup Global Preferences',
       cta: 'New dashboard',
       icon: 'icon-gf icon-gf-dashboard',
-      href: 'dashboard/new?gettingstarted',
+      href: 'datasources',
       check: () => {
         return this.backendSrv.search({ limit: 1 }).then(result => {
           return result.length > 0;
