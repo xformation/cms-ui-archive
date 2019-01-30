a7852c562d088862759ffab6699a6d794ee6b216
imran-synectiks <imran.mohd@synectiks.com>
2019-01-30 17:31:16 +0530

    changes

--- public/app/features/localapp/college-settings/generalinfo/GeneralInfoCtrl.ts
+++ public/app/features/localapp/college-settings/generalinfo/GeneralInfoCtrl.ts
@@ -1,15 +1,27 @@
 export class GeneralInfoCtrl {
+  colleges: any;
   navModel: any;
+  query: any;
   activeTabIndex = 0;
   logoSrc = '/public/img/logo-placeholder.png';
   bgSrc = '/public/img/dashboard.png';
-  $scope;
+  $scope: any;
   /** @ngInject */
-  constructor($scope) {
+  constructor($scope, private backendSrv) {
     this.activeTabIndex = 0;
     this.$scope = $scope;
+    this.query = '';
+    this.getColleges();
     $scope.getFile = this.getFile.bind(this);
     $scope.getbgFile = this.getbgFile.bind(this);
+    $scope.create = () => {
+      if (!$scope.collegeForm.$valid) {
+        return;
+      }
+      backendSrv.post('http://localhost:8080/api/colleges/', $scope.college).then(() => {
+        this.getColleges();
+      });
+    };
   }
 
   activateTab(tabIndex) {
@@ -39,5 +51,12 @@ export class GeneralInfoCtrl {
       this.$scope.$apply();
     };
     fileReader.readAsDataURL(file);
+  }
+
+  getColleges() {
+    this.backendSrv.get(`http://localhost:8080/api/colleges/`).then(result => {
+      this.colleges = result;
+      console.log(this.colleges);
+    });
   }
 }
