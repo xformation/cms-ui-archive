6f2a14b5a14a8694803186166aec69c7a0677827
imran-synectiks <imran.mohd@synectiks.com>
2019-02-22 17:57:34 +0530

    few changes

--- public/app/features/localapp/academic-settings/departmentsetup/DepartmentSetupCtrl.ts
+++ public/app/features/localapp/academic-settings/departmentsetup/DepartmentSetupCtrl.ts
@@ -1,13 +1,25 @@
+import { appEvents } from 'app/core/core';
+
 export class DepartmentSetupCtrl {
   departments: any;
   navModel: any;
+  query: any;
   activeTabIndex = 0;
-  $scope;
+  $scope: any;
   /** @ngInject */
   constructor($scope, private backendSrv) {
     this.activeTabIndex = 0;
-    this.$scope = $scope;
+    this.query = '';
     this.getDepartments();
+    this.$scope = $scope;
+    $scope.create = () => {
+      if (!$scope.departmentForm.$valid) {
+        return;
+      }
+      backendSrv.post('http://localhost:8080/api/departments/', $scope.department).then(() => {
+        this.getDepartments();
+      });
+    };
   }
 
   activateTab(tabIndex) {
@@ -17,6 +29,48 @@ export class DepartmentSetupCtrl {
   getDepartments() {
     this.backendSrv.get(`http://localhost:8080/api/departments/`).then(result => {
       this.departments = result;
+    });
+  }
+
+  deleteDepartment(department) {
+    appEvents.emit('confirm-modal', {
+      title: 'Delete',
+      text: 'Do you want to delete ' + department.name + '?',
+      icon: 'fa-trash',
+      yesText: 'Delete',
+      onConfirm: () => {
+        this.backendSrv.delete('http://localhost:8080/api/departments/' + department.id).then(() => {
+          this.getDepartments();
+        });
+      },
+    });
+  }
+
+  showModal() {
+    const text = 'Do you want to delete the ';
+
+    appEvents.emit('department-modal', {
+      text: text,
+      icon: 'fa-trash',
+      onCreate: (departmentForm, department) => {
+        this.$scope.departmentForm = departmentForm;
+        this.$scope.department = department;
+        this.$scope.create();
+      },
+    });
+  }
+
+  showImportModal() {
+    const text = 'Do you want to delete the ';
+
+    appEvents.emit('import-department-modal', {
+      text: text,
+      icon: 'fa-trash',
+      onCreate: (departmentForm, department) => {
+        this.$scope.departmentForm = departmentForm;
+        this.$scope.department = department;
+        this.$scope.create();
+      },
     });
   }
 }
