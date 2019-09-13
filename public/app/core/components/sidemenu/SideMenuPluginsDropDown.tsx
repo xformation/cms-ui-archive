0d0394f22ce3b26bb9a8a9f6ad361dd818ed0588
jasminmakasana <jasminmakasana1991@gmail.com>
2019-09-13 19:44:44 +0530

    Update: search implemented in side menu

--- public/app/core/components/sidemenu/SideMenuPluginsDropDown.tsx
+++ public/app/core/components/sidemenu/SideMenuPluginsDropDown.tsx
@@ -5,23 +5,42 @@ interface Props {
     links: any;
 }
 
-const SideMenuPluginsDropDown: SFC<Props> = props => {
-    const { links } = props;
-    return (
-        <div className="dropdown-menu dropdown-menu--sidemenu sidemenu-plugin-container" role="menu">
-            <div className="search-box-container">
-                <input type="text" className="gf-form-input search-box" placeHolder="Search Plugins" />
+class SideMenuPluginsDropDown extends React.Component {
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            searchItem: ""
+        };
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(e) {
+        const { name, value } = e.target;
+        this.setState({
+            [name]: value
+        });
+    }
+
+    render() {
+        const { links } = this.props;
+        const { searchItem } = this.state;
+        return (
+            <div className="dropdown-menu dropdown-menu--sidemenu sidemenu-plugin-container" role="menu">
+                <div className="search-box-container">
+                    <input type="text" className="gf-form-input search-box" placeHolder="Search Plugins" name="searchItem"
+                     value={searchItem} onChange={this.handleChange} />
+                </div>
+                <div className="plugins-wrapper">
+                    {links.map((link, index) => {
+                        {
+                            return (link.id.indexOf("plugin-page") !== -1 && link.text.indexOf(searchItem) !== -1 &&
+                                <PluginItem link={link} key={`${link.id}-${index}`} />);
+                        }
+                    })}
+                </div>
             </div>
-            <div className="plugins-wrapper">
-                {links.map((link, index) => {
-                    {
-                        return (link.id.indexOf("plugin-page") !== -1 &&
-                            <PluginItem link={link} key={`${link.id}-${index}`} />);
-                    }
-                })}
-            </div>
-        </div>
-    );
-};
+        );
+    }
+}
 
 export default SideMenuPluginsDropDown;
