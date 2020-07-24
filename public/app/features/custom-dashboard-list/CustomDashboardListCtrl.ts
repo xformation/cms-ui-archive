09499eeb3ab2235607806a182f6b0c625972a018
Jasmin Makasana <jasminmakasana1991@gmail.com>
2020-07-24 17:19:35 +0530

    Add: dashboard list folder tabs added.

--- public/app/features/custom-dashboard-list/CustomDashboardListCtrl.ts
+++ public/app/features/custom-dashboard-list/CustomDashboardListCtrl.ts
@@ -5,6 +5,8 @@ export class CustomDashboardListCtrl {
   sections: any[];
   isAsideCollapse: boolean;
   limit = 20;
+  activeSection: any;
+  activeDashboard: any;
   constructor(private backendSrv) {
     this.sections = [];
     this.getAllDashboards();
@@ -13,29 +15,42 @@ export class CustomDashboardListCtrl {
 
   getAllDashboards() {
     return this.backendSrv.search().then(result => {
-      this.buildSectionList(result);
+      this.sections = this.manipulateData(result);
+      const keys = Object.keys(this.sections);
+      if (keys && keys.length > 0) {
+        this.activeSection = this.sections[keys[0]];
+        const activeDashIndex = this.activeSection.activeDash;
+        this.activeDashboard = this.activeSection.dashboards[activeDashIndex];
+      }
     });
   }
 
-  buildSectionList(result) {
-    const length = result.length;
-    this.sections = [];
-    for (let i = 0; i < length; i++) {
-      const item = result[i];
-      const tempArr = item.url.split('/');
-      let slug = '';
-      if (tempArr.length === 3) {
-        slug = tempArr[2];
-      }
-      this.sections.push({
-        uid: item.uid,
-        slug,
-        title: item.title,
-      });
-      if (i === 0) {
-        this.viewId = item.uid;
+  setActiveSection(section) {
+    this.activeSection = section;
+    const activeDashIndex = section.activeDash;
+    this.activeDashboard = section.dashboards[activeDashIndex];
+  }
+
+  setActiveDashboard(dashboard, index) {
+    this.activeDashboard = dashboard;
+    this.activeSection.activeDash = index;
+  }
+
+  manipulateData(result: any) {
+    const retData: any = {};
+    for (let i = 0; i < result.length; i++) {
+      const dash = result[i];
+      if (dash.type === 'dash-db' && dash.folderId) {
+        retData[dash.folderId] = retData[dash.folderId] || { dashboards: [] };
+        retData[dash.folderId].title = dash.folderTitle;
+        retData[dash.folderId].folderId = dash.folderId;
+        if (retData[dash.folderId].dashboards.length === 0) {
+          retData[dash.folderId].activeDash = 0;
+        }
+        retData[dash.folderId].dashboards.push(dash);
       }
     }
+    return retData;
   }
 
   onClickLeftTab(section: any) {
